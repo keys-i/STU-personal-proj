31d9256ed0a40300d1433526d2d361d6cbf86d23
Keys <70819367+keys-i@users.noreply.github.com>
2026-01-08 15:05:48 +0700

    chore: change dynamic env resolution to simple url building
    Signed-off-by: Keys <70819367+keys-i@users.noreply.github.com>

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -19,6 +19,7 @@ async function bootstrap() {
       transform: true,
       whitelist: true,
       forbidNonWhitelisted: true,
+      transformOptions: { enableImplicitConversion: true },
     }),
   );
 
